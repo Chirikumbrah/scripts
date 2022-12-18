496ab40069d294c84da17fb36102fb65f62807bf
Chirikumbrah <basslo@protonmail.com>
2022-12-18 11:45:00 +0300

    removed unnesessary lines

--- post_install/scripts/pip_packages.sh
+++ post_install/scripts/pip_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pip install -U python-lsp-server python-config wxPython setuptools six wheel packaging pywal "python-lsp-server[all]"
+pip install -U python-config wxPython setuptools six wheel packaging pywal "python-lsp-server[all]"
              \ requests beautysh debugpy pip black flake8 djhtml curlylint pynvim jedi Django djangorestframework django-stubs
