707684e70b9f22a7507462519829efb41652774b
Chirikumbrah <basslo@protonmail.com>
2023-01-02 21:39:08 +0300

    added some useful functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,3 +2,48 @@
 function llama_launch () {
   cd "$(llama "$@")"
 }
+
+function fcd () {
+  if [ "$1" != '' ]; then
+    cd "$(fd . "$1" --hidden --type=directory | fzf)"
+  else
+    cd "$(fd --hidden --type=directory | fzf)"
+  fi
+}
+
+function edit () {
+  if [ "$1" != '' ]; then
+    FILE="$(fd . "$1" --hidden --type=file | fzf)"
+    if [ "$FILE" != '' ]; then
+      $EDITOR "$FILE"
+    else
+      echo "No file selected."
+    fi
+  else
+    FILE="$(fd --hidden --type=file | fzf)"
+    if [ "$FILE" != '' ]; then
+      $EDITOR "$FILE"
+    else
+      echo "No file selected."
+    fi
+  fi
+}
+
+function open () {
+  if [ "$1" != '' ]; then
+    FILE="$(fd . "$1" --hidden --type=file | fzf)"
+    if [ "$FILE" != '' ]; then
+      xdg-open "$FILE"
+    else
+      echo "No file selected."
+    fi
+  else
+    FILE="$(fd --hidden --type=file | fzf)"
+    if [ "$FILE" != '' ]; then
+      xdg-open "$FILE"
+    else
+      echo "No file selected."
+    fi
+  fi
+}
+
