8ab1df853b52131ef2870be535db4ee386d630b0
Chirikumbrah <basslo@protonmail.com>
2023-01-03 11:46:46 +0300

    added defaults.zsh

---
+++ zsh/defaults.zsh
@@ -0,0 +1,5 @@
+### DEFAULT APPLICATIONS ###
+xdg-mime default $IMAGE_VIEWER.desktop image/jpg
+xdg-mime default $IMAGE_VIEWER.desktop image/jpeg
+xdg-mime default $IMAGE_VIEWER.desktop image/png
+xdg-mime default $VIDEO_PLAYER.desktop image/mp4
