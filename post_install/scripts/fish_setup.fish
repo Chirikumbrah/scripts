7613bc9aaf6bb9cc70858c2701b484718976ae4f
Chirikumbrah <basslo@protonmail.com>
2023-01-08 11:59:21 +0300

    changed fish setup file

--- post_install/scripts/fish_setup.fish
+++ post_install/scripts/fish_setup.fish
@@ -1,5 +1,9 @@
 # !/bin/fish
 
+# change the default shell to fish
+/bin/bash -c chsh -s /bin/fish yr
+/bin/bash -c chsh -s /bin/fish root
+
 # install Fisher plugin manager
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install jorgebucaran/autopair.fish
