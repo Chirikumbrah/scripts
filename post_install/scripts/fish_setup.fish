94378ce3b52a735e966525390fef62bc024b7013
Chirikumbrah <basslo@protonmail.com>
2023-01-08 11:47:10 +0300

    added fish setup

---
+++ post_install/scripts/fish_setup.fish
@@ -0,0 +1,7 @@
+# !/bin/fish
+
+# install Fisher plugin manager
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/autopair.fish
+fisher install PatrickF1/fzf.fish
+fisher install dracula/fish
