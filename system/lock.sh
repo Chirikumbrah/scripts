73cb9f067862e0425bae618612a1b8daaf59257d
Chirikumbrah <basslo@protonmail.com>
2023-01-25 21:09:28 +0300

    beautyfied system scripts

--- system/lock.sh
+++ system/lock.sh
@@ -3,14 +3,15 @@
 set -e
 xset s off dpms 0 11 0
 
-# YR COLORS
-# i3lock --color=0f0210 --ignore-empty-password --show-failed-attempts \
-#        --nofork --clock  --keylayout 1 \
-#        --time-color=00ced1 --date-color=f6f454 --layout-color=ff00ff 
-# xset s off -dpms
-
 # DRACULA COLORS
-i3lock --color=282a36 --ignore-empty-password --show-failed-attempts \
-       --nofork --clock  --keylayout 1 \
-       --time-color=bd93f9 --date-color=f1fa8c --layout-color=50fa7b 
+i3lock --nofork                \
+       --clock                 \
+       --keylayout 1           \
+       --show-failed-attempts  \
+       --ignore-empty-password \
+       --color=282a36          \
+       --time-color=bd93f9     \
+       --date-color=f1fa8c     \
+       --layout-color=50fa7b   
+
 xset s off -dpms
--- system/take-screenshot.sh
+++ system/take-screenshot.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-
-SCREENSHOTS_PATH="$HOME/yrData/images/screenshots/'%d-%m-%Y_%H:%M:%S_$wx$h.png'"
-
-function partial_screenshot {
-    # scrot -z -s -d 1 ~/yrData/images/screenshots/'%d-%m-%Y_%H:%M:%S_$wx$h.png' && notify-send "Partial screenshot saved" -u low
-    flameshot gui
-}
-
-function full_screenshot {
-    # scrot -z -d 1 ~/yrData/images/screenshots/'%d-%m-%Y_%H:%M:%S_$wx$h.png' && notify-send "Screenshot saved" -u low
-    flameshot screen
-}
+#!/bin/sh
 
 case $1 in
-    full) full_screenshot ;;
-        
-    partial) partial_screenshot ;;
+    --full)    flameshot screen ;;
+    --partial) flameshot gui ;;
 esac
