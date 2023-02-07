a35194c94de387e1bf29866d227d766ed47c24b6
Chirikumbrah <basslo@protonmail.com>
2023-02-07 23:07:48 +0300

    added pipewire to the runit-daemons

--- post_install/scripts/adding-runit-daemons.sh
+++ post_install/scripts/adding-runit-daemons.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ln -s /etc/sv/{tlp,dbus,NetworkManager,chronyd,alsa,bluetoothd} /var/service/
+sudo ln -s /etc/sv/{tlp,dbus,NetworkManager,chronyd,alsa,pipewire,bluetoothd} /var/service/
