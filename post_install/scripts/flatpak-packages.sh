bde1a8db29d24d753790f29c4b48b0bf4052d5aa
Chirikumbrah <basslo@protonmail.com>
2023-02-28 13:10:56 +0300

    added mindustry flatpack package

--- post_install/scripts/flatpak-packages.sh
+++ post_install/scripts/flatpak-packages.sh
@@ -4,4 +4,5 @@ flatpak install com.discordapp.Discord \
       com.notesnook.Notesnook \
       net.jami.Jami \
       org.localsend.localsend_app \
-      net.cozic.joplin_desktop
+      net.cozic.joplin_desktop \
+      com.github.Anuken.Mindustry
